01c71b9e616a1c577053f59403bc037d47ba1693
deniseismo <denisseismo@gmail.com>
2020-09-05 12:32:15 +0700

    error handling for Discogs API; separate function for retrieving images via Discogs

--- uncover/helpers/discogs_api.py
+++ uncover/helpers/discogs_api.py
@@ -18,9 +18,19 @@ def get_album_id(album: str, artist: str):
     results = discogs.search(album, type='release', artist=artist)
     try:
         album_id = results[0].id
-    except KeyError:
+    except (KeyError, IndexError):
         return None
     return album_id
+
+
+def get_album_image(album_id: str):
+    if not album_id:
+        return None
+    try:
+        album_image = discogs.release(album_id).images[0]['uri']
+    except Exception:
+        return None
+    return album_image
 
 
 @timeit
@@ -45,8 +55,8 @@ def get_artist_top_albums_images_via_discogs(artist: str):
     album_info = {"info": artist, "albums": dict()}
     for album_title in album_titles:
         album_id = get_album_id(album_title, artist=artist)
-        album_image = discogs.release(album_id).images[0]['uri']
+        album_image = get_album_image(album_id)
         if album_image:
             album_info["albums"][album_title] = album_image
-    print(f'there are {len(album_info["albums"])} albums found with Spotify')
+    print(f'there are {len(album_info["albums"])} albums found with Discogs')
     return album_info
