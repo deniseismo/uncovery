b43163296391ff6afdc360f512894627670ac7d1
deniseismo <denisseismo@gmail.com>
2020-11-06 13:57:47 +0700

    fix finding specific album

--- uncover/helpers/main_async.py
+++ uncover/helpers/main_async.py
@@ -206,13 +206,13 @@ def sql_find_specific_album(artist_name: str, an_album_to_find: str):
     album_entries = Album.query.filter_by(artist=artist).all()
     album_found = None
     ratio_threshold = 94
-    an_album_to_find = an_album_to_find.lower()
+    an_album_to_find = utils.get_filtered_name(an_album_to_find)
     for album in album_entries:
-        album_title = album.title.lower()
+        current_album = utils.get_filtered_name(album.title)
         print(album.title, an_album_to_find)
         # implements a fuzzy match algorithm function
-        current_ratio = fuzz.ratio(album_title, an_album_to_find)
-        partial_ratio = fuzz.partial_ratio(album_title, an_album_to_find)
+        current_ratio = fuzz.ratio(current_album, an_album_to_find)
+        partial_ratio = fuzz.partial_ratio(current_album, an_album_to_find)
         if partial_ratio == 100:
             album_found = album.cover_art
         if current_ratio > 98:
