17fb285c43d19f134fb6117050aa185bb92cafe6
deniseismo <denisseismo@gmail.com>
2021-11-20 20:02:12 +0700

    render about page

---
+++ src/about.js
@@ -0,0 +1,74 @@
+import {frequentElements, removeAllChildNodes, insertAfter} from './utils.js'
+import {animateAboutItems, animatePlayButtons} from "./animation.js";
+import {removePlayButtons, removeAvatarContainer} from "./uiconfig.js";
+import {removeMusicInfoBox} from "./albuminfobox.js";
+
+
+// renders About 'Chapter'
+export function renderAboutPage() {
+  // remove unnecessary elements
+  removeAllChildNodes(frequentElements.gameFrame);
+  removeAvatarContainer();
+  removePlayButtons();
+  removeMusicInfoBox();
+  const searchBar = document.querySelector(".search-and-options-container");
+  searchBar.style.display = 'none';
+  // display 'vinyl' blocks
+  for (let i = 1; i < 10; i++) {
+    const frameItem = document.createElement('div');
+    frameItem.id = `about-${i}`;
+    frameItem.classList.add('about-item', "shadow-main");
+    // add circles inside vinyls
+    const centerCircle = document.createElement('div');
+    centerCircle.classList.add('center-circle');
+    frameItem.appendChild(centerCircle);
+    frequentElements.gameFrame.appendChild(frameItem);
+  }
+  // adds text
+  addAboutText();
+  // animate everything
+  animateAboutItems();
+};
+
+// adds About page text
+function addAboutText() {
+  const aboutText = document.createElement('div');
+  aboutText.classList.add('about-text');
+
+  const createdBy = document.createElement('p');
+  createdBy.classList.add('created-by');
+  const thanks = document.createElement('p');
+  thanks.classList.add('thanks');
+  const forWhat = document.createElement('p');
+  forWhat.classList.add('for-what');
+  const ifYouLike = document.createElement('p');
+  ifYouLike.classList.add('if-you-like');
+
+  createdBy.textContent = "Created by Denis Kostarev";
+  thanks.textContent = "A huge thanks goes out to The MetaBrainz Foundation, Last.fm, Discogs, and Spotify";
+  forWhat.textContent = "for their incredible resources.";
+  ifYouLike.textContent = "If you like this project, consider supporting it";
+
+
+  aboutText.appendChild(createdBy);
+  aboutText.appendChild(thanks);
+  aboutText.appendChild(forWhat);
+  aboutText.appendChild(ifYouLike);
+
+  frequentElements.gameFrame.appendChild(aboutText);
+
+  createDonateButtons();
+  animatePlayButtons('.play-button', 9);
+}
+
+function createDonateButtons() {
+  const buttonsContainer = document.createElement('div');
+  buttonsContainer.classList.add('flex-container', 'play-buttons-container');
+  const buyMeATea = document.createElement('a');
+  buyMeATea.classList.add("button", "btn", "shadow-main", "play-button", "visible");
+  buyMeATea.target = "_blank";
+  buyMeATea.href = "https://www.buymeacoffee.com/seismo";
+  buyMeATea.textContent = '🍵 BUY ME A TEA';
+  buttonsContainer.appendChild(buyMeATea);
+  insertAfter(buttonsContainer, frequentElements.gameFrame);
+}
