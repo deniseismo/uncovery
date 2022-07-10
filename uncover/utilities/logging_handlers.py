585d999417e8c5fd64346b2827cbaeb7c8096ab6
deniseismo <denisseismo@gmail.com>
2022-07-10 17:34:49 +0700

    add logger for arbitrary data

--- uncover/utilities/logging_handlers.py
+++ uncover/utilities/logging_handlers.py
@@ -1,4 +1,7 @@
 import csv
+import os
+
+from flask import current_app
 
 
 def log_artist_missing_from_db(artist_name: str):
@@ -19,3 +22,19 @@ def log_artist_missing_from_db(artist_name: str):
         writer = csv.writer(file)
         writer.writerow([artist_name])
         return True
+
+
+def log_arbitrary_data(data_to_log: str, filename: str):
+    """
+    :param data_to_log: any data you want to log in
+    :param filename: filename to save data into
+    :return:
+    """
+    filepath = os.path.join(current_app.root_path, 'dev/logs', filename)
+    try:
+        with open(filepath, 'a+', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
+            writer.writerow([data_to_log])
+    except IOError as e:
+        print('file not found', e)
+        return None
