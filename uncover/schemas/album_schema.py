91bdc9c9d61115aa9b601cbfb73382db0e4d8361
deniseismo <denisseismo@gmail.com>
2022-07-24 14:37:59 +0700

    add mbids, alt. name to AlbumInfo; minor changes to  __repr__

--- uncover/schemas/album_schema.py
+++ uncover/schemas/album_schema.py
@@ -11,17 +11,22 @@ class AlbumInfo(InfoBase):
     artist_names: list[str]
     names: list[str]
     rating: int
-    image: str
+    image: str = None
     image_small: str = None
     image_medium: str = None
     filtered_title: str = None
     id: int = 0
+    mbid: str = None
     spotify_name: str = None
+    alternative_name: str = None
     spotify_id: str = None
     release_date: datetime = None
 
     def __repr__(self):
-        return f"Album({self.title}) by Artist({self.artist_name}), {self.release_date}"
+        album_description = f"Album({self.title}) by Artist({self.artist_name})"
+        if self.release_date:
+            album_description += f", {self.release_date.year}"
+        return album_description
 
     def remove_duplicate_names(self):
         self.names = list(set(self.names))
