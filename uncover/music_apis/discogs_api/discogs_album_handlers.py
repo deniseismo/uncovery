2560c4ff7b7b7d1bee3ae52ab8361dce62d73e17
deniseismo <denisseismo@gmail.com>
2022-07-30 18:10:59 +0700

    fix expection handling for discogs api

--- uncover/music_apis/discogs_api/discogs_album_handlers.py
+++ uncover/music_apis/discogs_api/discogs_album_handlers.py
@@ -1,37 +1,54 @@
+from typing import Optional
+
+from discogs_client.exceptions import DiscogsAPIError
+
 from uncover import cache
 from uncover.music_apis.discogs_api.discogs_client_api import get_discogs_client
 
 
 @cache.memoize(timeout=3600)
-def get_album_discogs_id(album: str, artist: str):
+def get_album_discogs_id(album_name: str, artist_name: str):
     """
-    :param artist: artist's name
-    :param album: album name
+    :param artist_name: artist's name
+    :param album_name: album name
     :return: discogs ID for the album
     """
     discogs = get_discogs_client()
-    results = discogs.search(album, type='release', artist=artist)
     try:
-        album_id = results[0].id
-    except (KeyError, IndexError):
+        album_search_results = discogs.search(album_name, type='release', artist=artist_name)
+    except DiscogsAPIError as e:
+        print(e)
+        return None
+    try:
+        album_id = album_search_results[0].id
+    except (IndexError, AttributeError) as e:
+        print(e)
         return None
     return album_id
 
 
 @cache.memoize(timeout=3600)
-def discogs_get_album_image(album_discogs_id: str):
+def discogs_get_album_image(album_discogs_id: str) -> Optional[str]:
     """
+    get album image url given album id (album id on discogs)
     :param album_discogs_id: album's discogs id
-    :return:
+    :return: (str) album image url
     """
     discogs = get_discogs_client()
-    album_image = None
+    album_image_uri = None
     if not album_discogs_id:
         return None
     try:
-        album_images = discogs.release(album_discogs_id).images
-        if album_images:
-            album_image = album_images[0]['uri']
-    except (IndexError, KeyError, TypeError):
+        release_info = discogs.release(album_discogs_id)
+    except DiscogsAPIError as e:
+        print(e)
         return None
-    return album_image
+    try:
+        album_images = release_info.images
+        if album_images:
+            # the first image is is the primary image found on the Release page
+            album_image_uri = album_images[0]['uri']
+    except (AttributeError, KeyError, IndexError, TypeError) as e:
+        print(e)
+        return None
+    return album_image_uri
