55fb224fc0029a7af5959e4a5eb9264c15fb6742
deniseismo <denisseismo@gmail.com>
2022-08-14 21:55:39 +0700

    minor changes

--- uncover/dev/update_database/double_check_album_data/double_check_album_rating.py
+++ uncover/dev/update_database/double_check_album_data/double_check_album_rating.py
@@ -3,36 +3,34 @@ import time
 from tqdm import tqdm
 
 from uncover import db, create_app
-from uncover.music_apis.lastfm_api.lastfm_album_handlers import lastfm_get_album_listeners
 from uncover.models import Album
+from uncover.music_apis.lastfm_api.lastfm_album_handlers import lastfm_get_album_listeners
 
 app = create_app()
 app.app_context().push()
 
 
-def double_check_album_rating(album_start_id=0):
-    """double check album rating
+def update_album_ratings(album_start_id: int = 0) -> None:
+    """
+    updates all album ratings
+    :param album_start_id: (int) album id in db to start from
     """
     album_count = 0
     all_albums = Album.query.filter(Album.id > album_start_id).all()
-    for album in tqdm(all_albums):
+    for album_entry in tqdm(all_albums):
         print("-" * 10)
-
-        album_title = album.title.lower()
-        artist_name = album.artist.name
-        print(f"Checking Album({album_title}), id({album.id}) by Artist({artist_name})")
-        rating = lastfm_get_album_listeners(album_title, artist_name)
+        artist_name = album_entry.artist.name
+        print(f"Checking {album_entry}, id({album_entry.id})")
+        rating = lastfm_get_album_listeners(album_entry.title, artist_name)
         if not rating:
             continue
-        if album.rating != rating:
-            print(f"\n--{album_title}, {album.id} by {artist_name}--")
-            print(album.rating, "→", rating)
-            album.rating = rating
+        if album_entry.rating != rating:
+            print(f"\n--{album_entry}, {album_entry.id} --")
+            print(album_entry.rating, "→", rating)
+            album_entry.rating = rating
         album_count += 1
         time.sleep(1)
         if album_count % 30 == 0:
             time.sleep(5)
+
         db.session.commit()
-
-
-double_check_album_rating(37004)
